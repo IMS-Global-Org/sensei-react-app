373fc0588ac4fea505fc667a34a3e89dfa9664cb
IMS-Global <brennick.sci@gmail.com>
2017-12-08 02:20:06 -0700

    Bug Fixes for Heroku Upload

--- client/src/components/homepages/Home.js
+++ client/src/components/homepages/Home.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Grid } from 'semantic-ui-react';
 import Announcements from '../announcements/Announcements'
 import HomePage from './HomePage'
 
-class Home extends Component {
-  render() {
-    return(
-      <Container>
-        <Grid columns={2}>
-          <Grid.Column width={12}>
-            <HomePage />
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Announcements />
-          </Grid.Column>
-        </Grid>
-      </Container>
-    )
-  }
-}
+const Home = () => (
+  <Container>
+    <Grid columns={2}>
+      <Grid.Column width={12}>
+        <HomePage />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <Announcements />
+      </Grid.Column>
+    </Grid>
+  </Container>
+)
 
 export default Home;
