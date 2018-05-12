a14b46df9146cdf5d74291f49feb13b5cbe49d55
IMS-Global <brennick.sci@gmail.com>
2018-05-12 15:59:07 -0600

    REF Added New Home page background photos

--- client/src/components/homepages/Welcome.js
+++ client/src/components/homepages/Welcome.js
@@ -3,6 +3,7 @@ import '../../styles/parallax.css'
 
 const Welcome = () => (
   <div className='parallax-container'>
+    <div className='parallax-image form03' ></div>
     <div className='parallax-text'>
       <div className='parallax-header'>
         WELCOME...
@@ -11,7 +12,7 @@ const Welcome = () => (
         Bobby Lawrence Karate is the largest karate organization in the state. Our focus is to help each individual develop themselves as a whole and achieve their personal best.  Bobby Lawrence Karate blends the discipline associated with classical karate with kicking, punching and self-defense for enjoyable, fun, age appropriate programs for all. We have programs for Pre-school age children up to Adults. No previous experience needed.
       </p>
     </div>
-    <div className='parallax-image form02' ></div>
+    <div className='parallax-image form04' ></div>
     <div className='parallax-text'>
       <div className='parallax-header'>
         VISION
@@ -20,7 +21,7 @@ const Welcome = () => (
         The Bobby Lawrence Karate vision is to empower people's lives in our communities through the martial arts. Our goal is to help people be healthy, safe and instill the life skills and character of a true martial artist. We uphold and encourage important values like courtesy, patience, respect, self control and integrity.
       </p>
     </div>
-    <div className='parallax-image form03' ></div>
+    <div className='parallax-image form02' ></div>
     <div className='parallax-text'>
       <div className='parallax-header'>
         PURPOSE
@@ -29,7 +30,6 @@ const Welcome = () => (
         Bobby Lawrence Karate's purpose is to support our students in helping them develop confidence, focus, self discipline and self esteem through our martial arts instruction and true self defense. All the while, helping them be physically fit, which helps our students create a positive outlook, a spirit of constant improvement and an attitude that refuses to give up.
       </p>
     </div>
-    <div className='parallax-image form04' ></div>
   </div>
 )
 
